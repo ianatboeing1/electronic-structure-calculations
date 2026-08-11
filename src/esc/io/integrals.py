26258d2c014cd6775b72f8826bc658e1c9b8db0f
Ian Geiger <Mr.IanGeiger@MacBook-Pro-2.local>
2026-08-11 15:39:21 -0700

    Add kinetic energy matrix builder

--- src/esc/io/integrals.py
+++ src/esc/io/integrals.py
@@ -17,10 +17,25 @@ def read_overlap_matrix(filename):
             i, j, value = line.split()
             i, j = int(i) - 1, int(j) - 1
             value = float(value)
-            
+
             ao_matrix[i,j] = value
             ao_matrix[j,i] = value
-        print(ao_matrix)
+        return ao_matrix
+
+def read_kinetic_energy_matrix(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        nbf = int(lines[-1].split()[0]) if lines else None
+        ke_matrix = np.zeros((nbf,nbf))
+
+        for line in lines:
+            i, j, value = line.split()
+            i, j = int(i) - 1, int(j) - 1
+            value = float(value)
+
+            ke_matrix[i,j] = value
+            ke_matrix[j,i] = value
+        return(ke_matrix)
 
 #print(read_nuclear_repulsion("data/h2o-sto3g/enuc.dat"))
-print(read_overlap_matrix("data/h2o-sto3g/s.dat"))
+print(read_kinetic_energy_matrix("data/h2o-sto3g/s.dat"))
